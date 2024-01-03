799a847cfcc8894e701a0f4a99bec51349ceac67
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-04 00:09:22 +0100

    feat(community/nvim-notify): changed options

--- plugins/community.lua
+++ plugins/community.lua
@@ -228,6 +228,19 @@ add({
 	{ import = "astrocommunity.utility.noice-nvim" },
 	{
 		"folke/noice.nvim",
+		dependencies = {
+			-- credits: https://github.com/Uzaaft/nvim/blob/6a61a2269b860d392b0115cbf80b0d8ad414ad01/lua/plugins/init.lua#L12C2-L22
+			"rcarriga/nvim-notify",
+			---@type notify.Config
+			---@diagnostic disable-next-line: missing-fields
+			opts = {
+				timeout = 500,
+				render = "wrapped-compact",
+				stages = "slide",
+				fps = 144,
+				max_width = 70,
+			},
+		},
 		---@type NoiceConfig
 		opts = {
 			lsp = {
