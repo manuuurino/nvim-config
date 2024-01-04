0075872b3ce6c5141095e77e0d56e30e28888511
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-04 01:54:08 +0100

    feat(options): removed some already set by default

--- options.lua
+++ options.lua
@@ -4,29 +4,23 @@ local icons = require("user.icons")
 
 return {
 	opt = {
-		relativenumber = true,
-		number = true,
-		spell = true,
 		-- NOTE:
 		-- if you dont have the spells, you have to let neovim download them.
 		-- run this command `nvim --clean -c "set spelllang=en_us,de_de spell" -c "quit"`
+		spell = true,
 		spelllang = "en_us,de_de",
-		signcolumn = "auto",
-		wrap = false,
+
 		shiftwidth = 4,
 		tabstop = 4,
-		list = true,
-		expandtab = false,
+		list = true, -- render charaters
+		expandtab = false, -- use tabs instead of spaces
 		listchars = {
 			tab = icons.tab_char,
 			space = icons.space_char,
 		},
-		clipboard = "",
+		clipboard = "", -- no clipboard sharing
 		-- completeopt = "menu,menuone,noselect,noinsert",
-		foldcolumn = "auto:3",
-		foldlevelstart = 99,
-		foldenable = true,
-		foldmethod = "manual",
+		foldcolumn = "auto:3", -- show at least 3 fold lines
 		fillchars = {
 			foldopen = icons.expander_expanded,
 			foldsep = icons.expander_separator,
@@ -34,13 +28,10 @@ return {
 		},
 	},
 	g = {
-		mapleader = " ", -- sets vim.g.mapleader
-		autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-		cmp_enabled = true, -- enable completion at start
-		autopairs_enabled = true, -- enable autopairs at start
-		diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-		icons_enabled = not is_tty(), -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-		ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+		-- global options from astronvim: https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/options.lua
+		mapleader = " ",
+		icons_enabled = not is_tty(), -- disable icons in the UI
 		resession_enabled = true, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+		inlay_hints_enabled = false, -- enable or disable LSP inlay hints on startup (Neovim v0.10 only)
 	},
 }
