9f9ba277896e25782e290bdca65d454582676b1d
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-06 17:02:28 +0100

    feat(user/mappings): migrated from v3

---
+++ lua/plugins/user/mappings.lua
@@ -0,0 +1,70 @@
+---@type LazySpec
+return {
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		mappings = {
+			n = {
+				-- navigate buffer tabs with `H` and `L`
+				L = {
+					function()
+						require("astrocore.buffer").nav(
+							vim.v.count > 0 and vim.v.count or 1
+						)
+					end,
+					desc = "Next buffer",
+				},
+				H = {
+					function()
+						require("astrocore.buffer").nav(
+							-(vim.v.count > 0 and vim.v.count or 1)
+						)
+					end,
+					desc = "Previous buffer",
+				},
+
+				-- credits: https://github.com/t1gu1/astrovim-config/blob/5a1d7bd4933f8192de027071d62f953b4c831af3/mappings.lua#L74-L77
+				["<leader>pc"] = {
+					function()
+						vim.cmd.edit(
+							vim.fn.stdpath("config") .. "/lua/user/init.lua"
+						)
+						vim.cmd.cd("%:p:h")
+						-- HACK: without this, the path will not change. why though?
+						vim.cmd("Neotree")
+					end,
+					desc = "Open AstroVim User Config",
+				},
+
+				-- credits: https://github.com/ThePrimeagen/init.lua/blob/97c039bb88d8bbbcc9b1e3d0dc716a2ba202c6d2/lua/theprimeagen/remap.lua
+				["J"] = { "mzJ`z" },
+				["<C-d>"] = { "<C-d>zz", desc = "Scroll down" },
+				["<C-u>"] = { "<C-u>zz", desc = "Scroll up" },
+				["<C-o>"] = { "<C-o>zz", desc = "Jump backward" },
+				["<C-i>"] = { "<C-i>zz", desc = "Jump forward" },
+				["n"] = { "nzzzv" },
+				["N"] = { "Nzzzv" },
+				["Q"] = { "nop" },
+
+				-- yank to system clipboard
+				["<leader>y"] = { '"+y', desc = "Yank to system clipboard" },
+				["<leader>Y"] = { '"+Y', desc = "Yank from system clipboard" },
+
+				["<leader>."] = {
+					function()
+						vim.cmd.cd("%:p:h")
+						vim.notify(
+							"current working directory set to:\n"
+								.. vim.fn.getcwd()
+						)
+					end,
+					desc = "Set CWD",
+				},
+			},
+			t = {
+				-- setting a mapping to false will disable it
+				-- ["<esc>"] = false,
+			},
+		},
+	},
+}
