1eda56edd77db663447125ba1db67305dae3a169
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-06 17:03:10 +0100

    feat(user/mini.operators): migrated from v3

---
+++ lua/plugins/user/mini-operators.lua
@@ -0,0 +1,21 @@
+---@type LazySpec
+return {
+	"echasnovski/mini.operators",
+	dependencies = {
+		"AstroNvim/astrocore",
+		-- HACK: this overrides the neo-tree mapping
+		opts = function(_, opts)
+			local maps = opts.mappings
+			maps.n["<Leader>o"] = {
+				name = "Text edit operators",
+			}
+		end,
+	},
+	opts = {
+		evaluate = { prefix = "<leader>oe" },
+		exchange = { prefix = "<leader>ox" },
+		multiply = { prefix = "<leader>om" },
+		replace = { prefix = "<leader>or" },
+		sort = { prefix = "<leader>os" },
+	},
+}
