b704afd56ec7e2c3cab4f1a0cfcb801a6554fa7e
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-06 16:24:44 +0100

    feat(core/neo-tree): migrated from v3

---
+++ lua/plugins/astronvim/core-plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+---@type LazySpec
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = "AstroNvim/astroui",
+	opts = function(_, opts)
+		local get_icon = require("astroui").get_icon
+
+		return vim.tbl_deep_extend("force", opts, {
+			default_component_configs = {
+				indent = {
+					with_expanders = true,
+					expander_collapsed = get_icon("FoldClosed"),
+					expander_expanded = get_icon("FoldOpened"),
+					expander_highlight = "NeoTreeExpander",
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					always_show = {
+						".config",
+						".zshrc",
+						".env.zsh",
+						".zshenv",
+						".gitignore",
+					},
+				},
+			},
+		})
+	end,
+}
