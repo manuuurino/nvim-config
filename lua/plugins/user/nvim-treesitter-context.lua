7d460919e3a8a83f4f2163be6e8d8aba223d4be9
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-06 23:37:18 +0100

    feat(user/nvim-treesitter-context): migrated from v3

---
+++ lua/plugins/user/nvim-treesitter-context.lua
@@ -0,0 +1,20 @@
+---@type LazySpec
+return {
+	--TODO: add background color for tty
+	"nvim-treesitter/nvim-treesitter-context",
+	event = "User AstroFile",
+	dependencies = {
+		"AstroNvim/astrocore",
+		---@type AstroCoreOpts
+		opts = {
+			mappings = {
+				n = {
+					["<leader>uT"] = {
+						"<cmd>TSContextToggle<CR>",
+						desc = "Toggle treesitter context",
+					},
+				},
+			},
+		},
+	},
+}
