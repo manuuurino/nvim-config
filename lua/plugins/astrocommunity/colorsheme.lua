0392cdf58d4d38519fedb177ccd801af8f5e33c1
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-07 20:39:22 +0100

    feat(community/colorscheme): migrated from v3

---
+++ lua/plugins/astrocommunity/colorsheme.lua
@@ -0,0 +1,23 @@
+---@type LazySpec
+return {
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ "catppuccin/nvim", name = "catppuccin", event = "VeryLazy" },
+
+	{ import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+	{ "loctvl842/monokai-pro.nvim", event = "VeryLazy" },
+
+	{ import = "astrocommunity.colorscheme.dracula-nvim" },
+	{ "Mofiqul/dracula.nvim", event = "VeryLazy" },
+
+	{ import = "astrocommunity.colorscheme.gruvbox-nvim" },
+	{ "ellisonleao/gruvbox.nvim", event = "VeryLazy" },
+
+	{ import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+	{ "olimorris/onedarkpro.nvim", event = "VeryLazy" },
+
+	{ import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+	{ "nyoom-engineering/oxocarbon.nvim", event = "VeryLazy" },
+
+	{ import = "astrocommunity.colorscheme.kanagawa-nvim" },
+	{ "rebelot/kanagawa.nvim", event = "VeryLazy" },
+}
