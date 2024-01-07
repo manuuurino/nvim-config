ccb44d728f3aa0b33d75fdabce4c6ad3c016d196
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-08 00:15:08 +0100

    refactor(user/options): rename opt to o

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,20 +1,20 @@
 local is_tty = require("util.helper").is_tty
 
-local opt = vim.opt
+local o = vim.opt
 local g = vim.g
 
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.list = true -- render charaters
-opt.expandtab = false -- use tabs instead of spaces
-opt.clipboard = "" -- no clipboard sharing
+o.shiftwidth = 4
+o.tabstop = 4
+o.list = true -- render charaters
+o.expandtab = false -- use tabs instead of spaces
+o.clipboard = "" -- no clipboard sharing
 -- opt.completeopt = "menu,menuone,noselect,noinsert"
-opt.foldcolumn = "auto:3" -- show at least 3 fold lines
+o.foldcolumn = "auto:3" -- show at least 3 fold lines
 
 -- NOTE:if you dont have the spells, you have to let neovim download them.
 --      run this command `nvim --clean -c "set spelllang=en_us,de_de spell" -c "quit"`
-opt.spell = true
-opt.spelllang = { "en_us", "de_de" }
+o.spell = true
+o.spelllang = { "en_us", "de_de" }
 
 -- global options from astronvim:
 g.mapleader = " "
