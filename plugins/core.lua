36726a35dd8414e21e4ec176de166d2f1988657f
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-07 23:15:26 +0100

    feat(core/telescope): change layout

--- plugins/core.lua
+++ plugins/core.lua
@@ -104,9 +104,17 @@ local plugins = {
 		},
 		opts = {
 			defaults = {
+				sorting_strategy = "descending",
+				-- layout_strategy = "vertical",
 				layout_config = {
 					horizontal = {
 						prompt_position = "bottom",
+					},
+					vertical = {
+						height = 0.9,
+						preview_cutoff = 40,
+						prompt_position = "bottom",
+						width = 0.8,
 					},
 				},
 			},
