9c924eaa38c80bbdc006443b350712e8ba7c991c
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-01-15 21:23:07 +0100

    feat(astrocommunity/lsp): add inc-rename.nvim

--- lua/plugins/astrocommunity/lsp.lua
+++ lua/plugins/astrocommunity/lsp.lua
@@ -1,4 +1,21 @@
 ---@type LazySpec
 return {
+	{ import = "astrocommunity.lsp.inc-rename-nvim" },
+	{
+		"smjonas/inc-rename.nvim",
+		dependencies = {
+			"folke/noice.nvim",
+			---@type NoiceConfig
+			opts = {
+				cmdline = {
+					format = {
+						IncRename = {
+							view = "cmdline_popup",
+						},
+					},
+				},
+			},
+		},
+	},
 	{ import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
 }
