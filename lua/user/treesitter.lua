5eefc1da4c741a027b0b63775841eb9a9d35927c
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-02-12 02:06:55 +0100

    fix(user/treesitter): fix wrong list insertion

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,7 +3,9 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	---@param opts TSConfig
 	opts = function(_, opts)
-		opts.ensure_installed = require("astrocore").list_insert_unique({
+		opts.ensure_installed = require("astrocore").list_insert_unique(
+			---@diagnostic disable-next-line: param-type-mismatch
+			opts.ensure_installed,
 			"lua",
 			"vim",
 			"make",
@@ -11,7 +13,7 @@ return {
 			"git_config",
 			"passwd",
 			"rasi",
-			"scss",
-		}, opts.ensure_installed)
+			"scss"
+		)
 	end,
 }
