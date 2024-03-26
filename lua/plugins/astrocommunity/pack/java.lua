472dca33055ff3158f63e5b7b6de0b30dcbda4d9
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-03-26 23:31:55 +0100

    fix(java): fix formatter name from astrocommunity

--- lua/plugins/astrocommunity/pack/java.lua
+++ lua/plugins/astrocommunity/pack/java.lua
@@ -6,4 +6,27 @@ return {
 		import = "astrocommunity.pack.java",
 		cond = has("npm"),
 	},
+
+	-- TODO: this should be resolved in astrocommunity
+	{
+		"jay-babu/mason-null-ls.nvim",
+		optional = true,
+		opts = function(_, opts)
+			for k, v in pairs(opts.ensure_installed) do
+				if v == "clang_format" then
+					opts.ensure_installed[k] = "clang-format"
+					break
+				end
+			end
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		optional = true,
+		opts = {
+			formatters_by_ft = {
+				java = { "clang-format" },
+			},
+		},
+	},
 }
