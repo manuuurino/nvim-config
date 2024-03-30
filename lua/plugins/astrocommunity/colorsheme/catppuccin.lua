e7be81099095dd72bda85eed816a6e4afc7c5aa2
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-03-30 04:00:25 +0100

    feat(catppuccin): add `dim_inactive`

--- lua/plugins/astrocommunity/colorsheme/catppuccin.lua
+++ lua/plugins/astrocommunity/colorsheme/catppuccin.lua
@@ -1,5 +1,12 @@
 ---@type LazySpec
 return {
 	{ import = "astrocommunity.colorscheme.catppuccin" },
-	{ "catppuccin/nvim", name = "catppuccin", event = "VeryLazy" },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		event = "VeryLazy",
+		opts = {
+			dim_inactive = { enabled = true, percentage = 0.7 },
+		},
+	},
 }
