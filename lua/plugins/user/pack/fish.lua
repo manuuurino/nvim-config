793659f25f5307ab7a8ebb1ae75c9c8b0d740c22
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-03-30 02:56:40 +0100

    fix(fish): duplicated active sources when `nvim-lint` or `conform.nvim` enabled

--- lua/plugins/user/pack/fish.lua
+++ lua/plugins/user/pack/fish.lua
@@ -18,11 +18,19 @@ return {
 		optional = true,
 		opts = function(_, opts)
 			local nls = require("null-ls")
-			opts.sources = vim.list_extend(opts.sources or {}, {
-				nls.builtins.formatting.fish_indent,
-				nls.builtins.diagnostics.fish,
-			})
-			return opts
+			local is_available = require("astrocore").is_available
+
+			local sources = {}
+
+			if not is_available("conform.nvim") then
+				table.insert(sources, nls.builtins.formatting.fish_indent)
+			end
+
+			if not is_available("nvim-lint") then
+				table.insert(sources, nls.builtins.diagnostics.fish)
+			end
+
+			opts.sources = vim.list_extend(opts.sources or {}, sources)
 		end,
 	},
 	{
