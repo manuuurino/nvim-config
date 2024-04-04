22c16b0325ee1663f5231667d7065c7facd3714d
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-04 02:33:56 +0200

    refactor(lazy): made including astrocommunity plugin easier with fallback

--- lua/util/helper.lua
+++ lua/util/helper.lua
@@ -6,4 +6,13 @@ function M.spec(path)
 	return { import = path }
 end
 
+---@param category string
+---@param plugin_name string
+function M.astrocommunity(category, plugin_name)
+	local import = "astrocommunity." .. category .. "." .. plugin_name
+	local ok, plugin = pcall(require, "plugins." .. import)
+	if not ok then return { import = import } end
+	return plugin
+end
+
 return M
