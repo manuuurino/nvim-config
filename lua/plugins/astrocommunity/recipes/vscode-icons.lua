0378c508990550a451486e55de126afd1bdce795
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-09 19:45:58 +0200

    chore(vscode-icons): removed lspkind icons addition due PR got merged

--- lua/plugins/astrocommunity/recipes/vscode-icons.lua
+++ lua/plugins/astrocommunity/recipes/vscode-icons.lua
@@ -1,39 +1,4 @@
 ---@return LazySpec
 return {
 	{ import = "astrocommunity.recipes.vscode-icons" },
-	{
-		-- TODO: remove this snippet until this is merged: https://github.com/AstroNvim/astrocommunity/pull/851
-		"onsails/lspkind.nvim",
-		opts = function(_, opts)
-			opts.preset = "codicons"
-			opts.symbol_map = {
-				Text = " ",
-				Method = " ",
-				Function = " ",
-				Constructor = " ",
-				Field = " ",
-				Variable = " ",
-				Class = " ",
-				Interface = " ",
-				Module = " ",
-				Property = " ",
-				Unit = " ",
-				Value = " ",
-				Enum = " ",
-				Keyword = " ",
-				Snippet = " ",
-				Color = " ",
-				File = " ",
-				Reference = " ",
-				Folder = " ",
-				EnumMember = " ",
-				Constant = " ",
-				Struct = " ",
-				Event = " ",
-				Operator = " ",
-				TypeParameter = " ",
-			}
-			return opts
-		end,
-	},
 }
