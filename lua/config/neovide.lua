1b720008c2b8156aa7c2a0ff3eeced2469623039
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-09 18:48:30 +0200

    feat(neovide): adjusted transparency and blur

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -6,6 +6,10 @@ if not g.neovide then return end
 o.guifont = "SauceCodePro Nerd Font:h10"
 
 g.neovide_scale_factor = 1
-o.winblend = 20
-o.pumblend = 20
+o.winblend = 40
+o.pumblend = 40
+
+g.neovide_floating_blur_amount_x = 12.0
+g.neovide_floating_blur_amount_y = 12.0
+
 -- TODO: no TERM_PROGRAM env?
