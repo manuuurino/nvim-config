13c526dbcca035c7fb4ce58f506b113960f17c69
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-10 22:31:11 +0200

    perf(peek-nvim): load lazy

--- lua/plugins/astrocommunity/markdown-and-latex/peek-nvim.lua
+++ lua/plugins/astrocommunity/markdown-and-latex/peek-nvim.lua
@@ -2,6 +2,13 @@ local has = require("util.checks").has
 
 ---@type LazySpec
 return {
-	import = "astrocommunity.markdown-and-latex.peek-nvim",
-	cond = has("deno"),
+	{
+		import = "astrocommunity.markdown-and-latex.peek-nvim",
+		cond = has("deno"),
+	},
+	{
+		-- TODO: remove this after astrocommunity PR https://github.com/AstroNvim/astrocommunity/pull/867
+		"toppair/peek.nvim",
+		lazy = true,
+	},
 }
