1d2100921b4587b7c414297cf1b8fe538f07c4aa
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-10 22:13:38 +0200

    chore(peek-nvim): removed unnessecary spec

--- lua/plugins/astrocommunity/markdown-and-latex/peek-nvim.lua
+++ lua/plugins/astrocommunity/markdown-and-latex/peek-nvim.lua
@@ -2,12 +2,6 @@ local has = require("util.checks").has
 
 ---@type LazySpec
 return {
-	{
-		import = "astrocommunity.markdown-and-latex.peek-nvim",
-		cond = has("deno"),
-	},
-	{
-		"toppair/peek.nvim",
-		cmd = { "PeekOpen", "PeekClose" },
-	},
+	import = "astrocommunity.markdown-and-latex.peek-nvim",
+	cond = has("deno"),
 }
