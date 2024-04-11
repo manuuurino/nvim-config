5d728ecd9422d575211e9ca5849ac761a7dbe19e
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-11 07:07:45 +0200

    chore(blame-nvim): removed unneeded binding that got added in astrocommunity

--- lua/plugins/astrocommunity/git/blame-nvim.lua
+++ lua/plugins/astrocommunity/git/blame-nvim.lua
@@ -1,24 +1,4 @@
-local prefix = "<Leader>g"
-
-local mappings = {
-	n = {
-		[prefix .. "B"] = {
-			"<cmd>ToggleBlame<cr>",
-			desc = "Toggle git blame",
-		},
-	},
-}
 ---@type LazySpec
 return {
-	{ import = "astrocommunity.git.blame-nvim" },
-	{
-		"FabijanZulj/blame.nvim",
-		dependencies = {
-			"AstroNvim/astrocore",
-			---@type AstroCoreOpts
-			opts = {
-				mappings = mappings,
-			},
-		},
-	},
+	import = "astrocommunity.git.blame-nvim",
 }
