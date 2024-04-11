3fbdd6da9453e7731349ef9b0bff9f3383497e8f
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-11 07:08:25 +0200

    chore(refactoring-nvim): removed unneeded binding that got added in astrocommunity

--- lua/plugins/astrocommunity/editing-support/refactoring-nvim.lua
+++ lua/plugins/astrocommunity/editing-support/refactoring-nvim.lua
@@ -1,25 +1,4 @@
-local prefix = "<Leader>r"
-
-local mappings = {
-	n = {
-		[prefix] = { name = "Refactor" },
-	},
-	x = {
-		[prefix] = { name = "Refactor" },
-	},
-}
-
 ---@type LazySpec
 return {
-	{ import = "astrocommunity.editing-support.refactoring-nvim" },
-	{
-		"ThePrimeagen/refactoring.nvim",
-		dependencies = {
-			"AstroNvim/astrocore",
-			---@type AstroCoreOpts
-			opts = {
-				mappings = mappings,
-			},
-		},
-	},
+	import = "astrocommunity.editing-support.refactoring-nvim",
 }
