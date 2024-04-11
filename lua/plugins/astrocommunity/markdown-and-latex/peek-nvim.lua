e68fd421f86867d35182ab611c7b767bc76ebe25
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-11 07:06:55 +0200

    chore(peek-nvim): removed temporary fix that got solved in astrocommunity

--- lua/plugins/astrocommunity/markdown-and-latex/peek-nvim.lua
+++ lua/plugins/astrocommunity/markdown-and-latex/peek-nvim.lua
@@ -6,9 +6,4 @@ return {
 		import = "astrocommunity.markdown-and-latex.peek-nvim",
 		cond = has("deno"),
 	},
-	{
-		-- TODO: remove this after astrocommunity PR https://github.com/AstroNvim/astrocommunity/pull/867
-		"toppair/peek.nvim",
-		lazy = true,
-	},
 }
