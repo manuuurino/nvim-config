366b8005d71c1e8f8adf4a79c581156d20e07040
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-14 01:53:11 +0200

    feat(todo-comments-nvim): add mappings for `trouble.nvim`

--- lua/plugins/astronvim/todo-comments-nvim.lua
+++ lua/plugins/astronvim/todo-comments-nvim.lua
@@ -10,10 +10,15 @@ return {
 				local maps_n = opts.mappings.n
 				local is_available = require("astrocore").is_available
 
-				if is_available("trouble-nvim") then
+				-- TODO: remove this once this PR is merged: https://github.com/AstroNvim/astrocommunity/pull/879
+				if is_available("trouble.nvim") then
 					maps_n["<Leader>xt"] = {
 						"<cmd>TodoTrouble<cr>",
-						desc = "Todo trouble",
+						desc = "Todo (Trouble)",
+					}
+					maps_n["<Leader>xT"] = {
+						"<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",
+						desc = "Todo/Fix/Fixme (Trouble)",
 					}
 				end
 			end,
