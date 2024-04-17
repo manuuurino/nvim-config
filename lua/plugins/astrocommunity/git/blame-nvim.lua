1b9f1c56eac878b1d866a4a48ebb64c12c01af5d
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-17 18:19:50 +0200

    fix(blame-nvim): adjust to the new rewrite

--- lua/plugins/astrocommunity/git/blame-nvim.lua
+++ lua/plugins/astrocommunity/git/blame-nvim.lua
@@ -1,4 +1,53 @@
 ---@type LazySpec
 return {
-	import = "astrocommunity.git.blame-nvim",
+	-- {
+	-- 	import = "astrocommunity.git.blame-nvim",
+	-- },
+	-- TODO: remove this temporary fix after this PR has been merged: https://github.com/AstroNvim/astrocommunity/pull/892
+	{
+		"FabijanZulj/blame.nvim",
+		cmd = "BlameToggle",
+		config = function(_, opts)
+			require("blame").setup(opts)
+		end,
+		dependencies = {
+			{
+				"AstroNvim/astrocore",
+				---@type AstroCoreOpts
+				opts = {
+					mappings = {
+						n = {
+							["<Leader>gB"] = {
+								"<cmd>BlameToggle<cr>",
+								desc = "Toggle git blame",
+							},
+						},
+					},
+				},
+			},
+			{ -- configure heirline with custom hook to enable winbar for 'blame' filetype
+				"rebelot/heirline.nvim",
+				optional = true,
+				opts = function(_, opts)
+					local disable_winbar_cb =
+						vim.tbl_get(opts, "opts", "disable_winbar_cb")
+					return require("astrocore").extend_tbl(opts, {
+						opts = {
+							disable_winbar_cb = function(args)
+								if
+									vim.api.nvim_buf_is_valid(args.buf)
+									and vim.bo[args.buf].filetype == "blame"
+								then
+									return false
+								end
+								if disable_winbar_cb then
+									return disable_winbar_cb(args)
+								end
+							end,
+						},
+					})
+				end,
+			},
+		},
+	},
 }
