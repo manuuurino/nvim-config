d7441f6197ca69231ee48353ab17fb5349eb1d4b
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-23 20:00:10 +0200

    chore(spec): add description and missing types to a function

--- lua/util/spec.lua
+++ lua/util/spec.lua
@@ -6,8 +6,12 @@ function M.spec(path)
 	return { import = path }
 end
 
+--- Loads a plugin from 'astrocommunity' or a local override based on category and plugin name which lies in 'plugins'.
+--- Constructs a module path and tries to load it. If the import is unavailable, it returns the attempted import.
+---
 ---@param category string
 ---@param plugin_name string
+---@return LazySpec
 function M.astrocommunity(category, plugin_name)
 	local import = "astrocommunity." .. category .. "." .. plugin_name
 	local ok, plugin = pcall(require, "plugins." .. import)
