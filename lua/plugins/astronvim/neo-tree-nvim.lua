ce17aee2a39b4737880563961c14dc52c864afa4
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-06-06 05:21:38 +0200

    chore(neo-tree-nvim): use table for mapping instead

--- lua/plugins/astronvim/neo-tree-nvim.lua
+++ lua/plugins/astronvim/neo-tree-nvim.lua
@@ -5,12 +5,15 @@ return {
 		"AstroNvim/astroui",
 		{
 			"AstroNvim/astrocore",
-			---@param opts AstroCoreOpts
-			-- HACK: AstroNvim uses a function to setup the opts, so we need to use it here too
-			opts = function(_, opts)
+			---@type AstroCoreOpts
+			opts = {
 				-- disable AstroNvim Toggle Explorer Focus
-				opts.mappings.n["<Leader>o"] = false
-			end,
+				mappings = {
+					n = {
+						["<Leader>o"] = false,
+					},
+				},
+			},
 		},
 	},
 	opts = function(_, opts)
