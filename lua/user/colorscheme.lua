0d17c18a820e3a1ad6fb5bbb8567aa9196d9e2d0
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-06-16 16:28:32 +0200

    fix(colorscheme): no colorscheme for tty

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,7 +5,11 @@ return {
 	---@param opts AstroUIOpts
 	opts = function(_, opts)
 		local is_available = require("astrocore").is_available
-		opts.colorscheme = is_available("catppuccin") and "catppuccin-mocha"
-			or "astrodark"
+		if require("util.checks").is_tty() then
+			opts.colorscheme = "elflord"
+		else
+			opts.colorscheme = is_available("catppuccin") and "catppuccin-mocha"
+				or "astrodark"
+		end
 	end,
 }
