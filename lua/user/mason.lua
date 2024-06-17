445b43ec0ea151b7223c18ed5af1c40073970b27
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-06-17 23:20:16 +0200

    feat(mason): make use of `opts_extend` in spec

--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -4,44 +4,23 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		optional = true,
 		---@param opts MasonLspconfigSettings
-		opts = function(_, opts)
-			---@diagnostic disable-next-line: inject-field
-			opts.ensure_installed = require("astrocore").list_insert_unique(
-				opts.ensure_installed,
-				{}
-			)
-		end,
+		opts = { ensure_installed = {} },
 	},
 	{
 		"jay-babu/mason-null-ls.nvim",
 		optional = true,
 		---@param opts MasonNullLsSettings
-		opts = function(_, opts)
-			opts.ensure_installed = require("astrocore").list_insert_unique(
-				opts.ensure_installed,
-				{}
-			)
-		end,
+		opts = { ensure_installed = {} },
 	},
 	{
 		"jay-babu/mason-nvim-dap.nvim",
 		optional = true,
 		---@param opts MasonNvimDapSettings
-		opts = function(_, opts)
-			opts.ensure_installed = require("astrocore").list_insert_unique(
-				opts.ensure_installed,
-				{}
-			)
-		end,
+		opts = { ensure_installed = {} },
 	},
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		optional = true,
-		opts = function(_, opts)
-			opts.ensure_installed = require("astrocore").list_insert_unique(
-				opts.ensure_installed,
-				{}
-			)
-		end,
+		opts = { ensure_installed = {} },
 	},
 }
