f2c8082a14ea144bcc135843030d1e13eb0b9b2d
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-06-25 22:28:36 +0200

    refactor(treesitter): make use of `opts_extend` in spec

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,22 +1,18 @@
 ---@type LazySpec
 return {
 	"nvim-treesitter/nvim-treesitter",
-	---@param opts TSConfig
-	opts = function(_, opts)
-		opts.ensure_installed = require("astrocore").list_insert_unique(
-			---@diagnostic disable-next-line: param-type-mismatch
-			opts.ensure_installed,
-			{
-				"lua",
-				"vim",
-				"make",
-				"gitignore",
-				"git_config",
-				"passwd",
-				"rasi",
-				"scss",
-				"fish",
-			}
-		)
-	end,
+	---@type TSConfig
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vim",
+			"make",
+			"gitignore",
+			"git_config",
+			"passwd",
+			"rasi",
+			"scss",
+			"fish",
+		},
+	},
 }
