b20c507889800203a03323add622d457e205fa80
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-07-04 08:48:44 +0200

    feat(grug-far-nvim): use spec from astrocommunity

--- lua/plugins/user/grug-far-nvim.lua
+++ lua/plugins/user/grug-far-nvim.lua
@@ -1,30 +1,51 @@
-local prefix = "<Leader>f"
-local mappings = {
-	n = {
-		[prefix .. "R"] = {
-			function()
-				require("grug-far").grug_far({
-					prefills = { search = vim.fn.expand("<cword>") },
-				})
-			end,
-			desc = "Search and Replace",
-		},
-	},
-}
-
+-- credits: https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/project/grug-far-nvim/init.lua
 ---@type LazySpec
 return {
 	"MagicDuck/grug-far.nvim",
 	cmd = "GrugFar",
-	---@type GrugFarOptions
-	opts = {
-		maxWorkers = 8,
-	},
 	dependencies = {
-		"AstroNvim/astrocore",
-		---@type AstroCoreOpts
-		opts = {
-			mappings = mappings,
+		{
+			"AstroNvim/astroui",
+			---@type AstroUIOpts
+			opts = {
+				icons = {
+					GrugFar = "󰛔",
+				},
+			},
+		},
+		{
+			"AstroNvim/astrocore",
+			---@param opts AstroCoreOpts
+			opts = function(_, opts)
+				local maps = opts.mappings and opts.mappings
+					or require("astrocore").empty_map_table()
+				local prefix = "<Leader>fR"
+
+				maps.n[prefix] = {
+					function()
+						require("grug-far").grug_far({})
+					end,
+					desc = require("astroui").get_icon("GrugFar", 1, true)
+						.. "Search and Replace",
+				}
+
+				maps.x[prefix] = {
+					function()
+						require("grug-far").with_visual_selection({})
+					end,
+					desc = require("astroui").get_icon("GrugFar", 1, true)
+						.. "Search and Replace (current word)",
+				}
+			end,
 		},
 	},
+	---@param opts GrugFarOptionsOverride
+	-- NOTE: Wrapping opts into a function, because `astrocore` can set vim options
+	opts = function(_, opts)
+		return require("astrocore").extend_tbl(opts, {
+			icons = {
+				enabled = vim.g.icons_enabled,
+			},
+		} --[[@as GrugFarOptionsOverride]])
+	end,
 }
