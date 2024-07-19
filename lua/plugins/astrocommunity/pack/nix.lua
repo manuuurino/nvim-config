d3310a3efa4925a41c186ff5bca6092cd8746aeb
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-07-19 05:20:41 +0200

    feat(nix): add needed package conditions

--- lua/plugins/astrocommunity/pack/nix.lua
+++ lua/plugins/astrocommunity/pack/nix.lua
@@ -3,5 +3,10 @@ local has = require("util.checks").has
 ---@type LazySpec
 return {
 	import = "astrocommunity.pack.nix",
-	cond = has("cargo"),
+	cond = has("cargo")
+		and has("nixd")
+		-- and has("nixfmt") -- TODO: nixfmt some day is getting added in the pack
+		and has("alejandra")
+		and has("deadnix")
+		and has("statix"),
 }
