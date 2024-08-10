a7dbe8227440b15980c9b47590ab1d1e2300f4e9
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-08-11 01:16:54 +0200

    feat(nix): set lsp ignore on `sema-escaping-with`

--- lua/plugins/astrocommunity/pack/nix.lua
+++ lua/plugins/astrocommunity/pack/nix.lua
@@ -2,11 +2,31 @@ local has = require("util.checks").has
 
 ---@type LazySpec
 return {
-	import = "astrocommunity.pack.nix",
-	cond = has("cargo")
-		and has("nixd")
-		-- and has("nixfmt") -- TODO: nixfmt some day is getting added in the pack
-		and has("alejandra")
-		and has("deadnix")
-		and has("statix"),
+	{
+		import = "astrocommunity.pack.nix",
+		cond = has("cargo")
+			and has("nixd")
+			-- and has("nixfmt") -- TODO: nixfmt some day is getting added in the pack
+			and has("alejandra")
+			and has("deadnix")
+			and has("statix"),
+	},
+	{
+		"AstroNvim/astrolsp",
+		---@type AstroLSPOpts
+		opts = {
+			---@diagnostic disable-next-line: missing-fields
+			config = {
+				nixd = {
+					settings = {
+						nixd = {
+							diagnostics = {
+								suppress = { "sema-escaping-with" },
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 }
